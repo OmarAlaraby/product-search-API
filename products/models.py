321e2a241408d348ba38c47d7f33b794437e4efb
OmarAlaraby <omar.alaraby23@gmail.com>
2025-05-17 17:44:43 +0300

    added nutrition facts to the product model

--- products/models.py
+++ products/models.py
@@ -21,6 +21,31 @@ class Product(models.Model):
     brand = models.ForeignKey(ProductBrand, on_delete=models.CASCADE)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    serving_size = models.CharField(max_length=100, blank=True, null=True)
+    servings_per_container = models.FloatField(blank=True, null=True)
+    calories = models.FloatField(blank=True, null=True)
+    total_fat = models.FloatField(blank=True, null=True)
+    saturated_fat = models.FloatField(blank=True, null=True)
+    trans_fat = models.FloatField(blank=True, null=True)
+    cholesterol = models.FloatField(blank=True, null=True)
+    sodium = models.FloatField(blank=True, null=True)
+    total_carbohydrates = models.FloatField(blank=True, null=True)
+    dietary_fiber = models.FloatField(blank=True, null=True)
+    sugars = models.FloatField(blank=True, null=True)
+    added_sugars = models.FloatField(blank=True, null=True)
+    protein = models.FloatField(blank=True, null=True)
+    vitamin_d = models.FloatField(blank=True, null=True)
+    calcium = models.FloatField(blank=True, null=True)
+    iron = models.FloatField(blank=True, null=True)
+    potassium = models.FloatField(blank=True, null=True)
+
+    ingredients = models.TextField(blank=True, null=True)
+    allergens = models.TextField(blank=True, null=True)
+
+    is_vegan = models.BooleanField(default=False)
+    is_gluten_free = models.BooleanField(default=False)
+    is_organic = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -2,9 +2,45 @@ from rest_framework import viewsets
 from .models import Product, ProductBrand, ProductCategory
 from .serializers import ProductSerializer, ProductBrandSerializer, ProductCategorySerializer
 
+# utils
+from django.db.models import Q
+from decouple import config
+import deepl
+from functools import reduce
+import operator
+from langdetect import detect
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.query_params.get('search', None)
+
+        if keyword:
+            translator = deepl.Translator(config('DEEPL_API_KEY'))
+            translated = translator.translate_text(keyword, target_lang="EN-US")
+
+            print(keyword, translated)
+            search_fields = [
+                "name", "description", "brand__name", "brand__description", "category__name",
+                "category__description", "ingredients", "allergens", "serving_size", "calories", "total_fat",
+                "saturated_fat", "trans_fat", "cholesterol", "sodium", "total_carbohydrates", "dietary_fiber",
+                "sugars", "added_sugars", "protein", "vitamin_d", "calcium", "iron", "potassium",
+            ]
+
+            
+            queries = [
+                Q(**{f"{field}__icontains": keyword}) | Q(**{f"{field}__icontains": translated})
+                for field in search_fields
+            ]
+            
+            combined_query = reduce(operator.or_, queries)
+            queryset = queryset.filter(combined_query).distinct()
+
+        return queryset
+
 
 class ProductBrandViewSet(viewsets.ModelViewSet):
     queryset = ProductBrand.objects.all()
